0e9044c2f876a549b5c026eaf8e0c498d9923671
Mauricio Valencia Cosme <mauricio.valencia@pragma.com.co>
2020-08-20 20:39:28 -0500

    create from

---
+++ Plantilla_Sistema_Facturacion/FrmCustomer.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace Plantilla_Sistema_Facturacion
+{
+    partial class FrmCustomer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtNombreCliente = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.LblAdministracionDeClientes = new MaterialSkin.Controls.MaterialLabel();
+            this.txtDocumento = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.TxtDireccion = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.TxtTelefono = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.TxtEmail = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.BtnNuevo = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.BtnActualizar = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.BtnEliminar = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.TxtBuscarCliente = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.BtnBuscar = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.TxtEmail);
+            this.panel1.Controls.Add(this.TxtTelefono);
+            this.panel1.Controls.Add(this.TxtDireccion);
+            this.panel1.Controls.Add(this.txtDocumento);
+            this.panel1.Controls.Add(this.txtNombreCliente);
+            this.panel1.Location = new System.Drawing.Point(71, 59);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(601, 263);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtNombreCliente
+            // 
+            this.txtNombreCliente.Depth = 0;
+            this.txtNombreCliente.Hint = "Nombre cliente";
+            this.txtNombreCliente.Location = new System.Drawing.Point(37, 25);
+            this.txtNombreCliente.MaxLength = 32767;
+            this.txtNombreCliente.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtNombreCliente.Name = "txtNombreCliente";
+            this.txtNombreCliente.PasswordChar = '\0';
+            this.txtNombreCliente.SelectedText = "";
+            this.txtNombreCliente.SelectionLength = 0;
+            this.txtNombreCliente.SelectionStart = 0;
+            this.txtNombreCliente.Size = new System.Drawing.Size(363, 23);
+            this.txtNombreCliente.TabIndex = 2;
+            this.txtNombreCliente.TabStop = false;
+            this.txtNombreCliente.UseSystemPasswordChar = false;
+            // 
+            // LblAdministracionDeClientes
+            // 
+            this.LblAdministracionDeClientes.AutoSize = true;
+            this.LblAdministracionDeClientes.Depth = 0;
+            this.LblAdministracionDeClientes.Font = new System.Drawing.Font("Roboto", 11F);
+            this.LblAdministracionDeClientes.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.LblAdministracionDeClientes.Location = new System.Drawing.Point(375, 9);
+            this.LblAdministracionDeClientes.MouseState = MaterialSkin.MouseState.HOVER;
+            this.LblAdministracionDeClientes.Name = "LblAdministracionDeClientes";
+            this.LblAdministracionDeClientes.Size = new System.Drawing.Size(229, 19);
+            this.LblAdministracionDeClientes.TabIndex = 1;
+            this.LblAdministracionDeClientes.Text = "ADMINISTRACIÓN DE CLIENTES";
+            // 
+            // txtDocumento
+            // 
+            this.txtDocumento.Depth = 0;
+            this.txtDocumento.Hint = "Documento";
+            this.txtDocumento.Location = new System.Drawing.Point(37, 76);
+            this.txtDocumento.MaxLength = 32767;
+            this.txtDocumento.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtDocumento.Name = "txtDocumento";
+            this.txtDocumento.PasswordChar = '\0';
+            this.txtDocumento.SelectedText = "";
+            this.txtDocumento.SelectionLength = 0;
+            this.txtDocumento.SelectionStart = 0;
+            this.txtDocumento.Size = new System.Drawing.Size(363, 23);
+            this.txtDocumento.TabIndex = 3;
+            this.txtDocumento.TabStop = false;
+            this.txtDocumento.UseSystemPasswordChar = false;
+            // 
+            // TxtDireccion
+            // 
+            this.TxtDireccion.Depth = 0;
+            this.TxtDireccion.Hint = "Dirección";
+            this.TxtDireccion.Location = new System.Drawing.Point(37, 121);
+            this.TxtDireccion.MaxLength = 32767;
+            this.TxtDireccion.MouseState = MaterialSkin.MouseState.HOVER;
+            this.TxtDireccion.Name = "TxtDireccion";
+            this.TxtDireccion.PasswordChar = '\0';
+            this.TxtDireccion.SelectedText = "";
+            this.TxtDireccion.SelectionLength = 0;
+            this.TxtDireccion.SelectionStart = 0;
+            this.TxtDireccion.Size = new System.Drawing.Size(363, 23);
+            this.TxtDireccion.TabIndex = 4;
+            this.TxtDireccion.TabStop = false;
+            this.TxtDireccion.UseSystemPasswordChar = false;
+            // 
+            // TxtTelefono
+            // 
+            this.TxtTelefono.Depth = 0;
+            this.TxtTelefono.Hint = "Teléfono";
+            this.TxtTelefono.Location = new System.Drawing.Point(37, 163);
+            this.TxtTelefono.MaxLength = 32767;
+            this.TxtTelefono.MouseState = MaterialSkin.MouseState.HOVER;
+            this.TxtTelefono.Name = "TxtTelefono";
+            this.TxtTelefono.PasswordChar = '\0';
+            this.TxtTelefono.SelectedText = "";
+            this.TxtTelefono.SelectionLength = 0;
+            this.TxtTelefono.SelectionStart = 0;
+            this.TxtTelefono.Size = new System.Drawing.Size(363, 23);
+            this.TxtTelefono.TabIndex = 5;
+            this.TxtTelefono.TabStop = false;
+            this.TxtTelefono.UseSystemPasswordChar = false;
+            // 
+            // TxtEmail
+            // 
+            this.TxtEmail.Depth = 0;
+            this.TxtEmail.Hint = "Email";
+            this.TxtEmail.Location = new System.Drawing.Point(37, 208);
+            this.TxtEmail.MaxLength = 32767;
+            this.TxtEmail.MouseState = MaterialSkin.MouseState.HOVER;
+            this.TxtEmail.Name = "TxtEmail";
+            this.TxtEmail.PasswordChar = '\0';
+            this.TxtEmail.SelectedText = "";
+            this.TxtEmail.SelectionLength = 0;
+            this.TxtEmail.SelectionStart = 0;
+            this.TxtEmail.Size = new System.Drawing.Size(363, 23);
+            this.TxtEmail.TabIndex = 6;
+            this.TxtEmail.TabStop = false;
+            this.TxtEmail.UseSystemPasswordChar = false;
+            // 
+            // BtnNuevo
+            // 
+            this.BtnNuevo.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BtnNuevo.Depth = 0;
+            this.BtnNuevo.Icon = null;
+            this.BtnNuevo.Location = new System.Drawing.Point(704, 59);
+            this.BtnNuevo.MouseState = MaterialSkin.MouseState.HOVER;
+            this.BtnNuevo.Name = "BtnNuevo";
+            this.BtnNuevo.Primary = true;
+            this.BtnNuevo.Size = new System.Drawing.Size(103, 36);
+            this.BtnNuevo.TabIndex = 2;
+            this.BtnNuevo.Text = "Nuevo";
+            this.BtnNuevo.UseVisualStyleBackColor = true;
+            // 
+            // BtnActualizar
+            // 
+            this.BtnActualizar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BtnActualizar.Depth = 0;
+            this.BtnActualizar.Icon = null;
+            this.BtnActualizar.Location = new System.Drawing.Point(704, 122);
+            this.BtnActualizar.MouseState = MaterialSkin.MouseState.HOVER;
+            this.BtnActualizar.Name = "BtnActualizar";
+            this.BtnActualizar.Primary = true;
+            this.BtnActualizar.Size = new System.Drawing.Size(103, 36);
+            this.BtnActualizar.TabIndex = 3;
+            this.BtnActualizar.Text = "Actualizar";
+            this.BtnActualizar.UseVisualStyleBackColor = true;
+            // 
+            // BtnEliminar
+            // 
+            this.BtnEliminar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BtnEliminar.Depth = 0;
+            this.BtnEliminar.Icon = null;
+            this.BtnEliminar.Location = new System.Drawing.Point(704, 193);
+            this.BtnEliminar.MouseState = MaterialSkin.MouseState.HOVER;
+            this.BtnEliminar.Name = "BtnEliminar";
+            this.BtnEliminar.Primary = true;
+            this.BtnEliminar.Size = new System.Drawing.Size(103, 36);
+            this.BtnEliminar.TabIndex = 4;
+            this.BtnEliminar.Text = "Eliminar";
+            this.BtnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // TxtBuscarCliente
+            // 
+            this.TxtBuscarCliente.Depth = 0;
+            this.TxtBuscarCliente.Hint = "Buscar cliente";
+            this.TxtBuscarCliente.Location = new System.Drawing.Point(37, 25);
+            this.TxtBuscarCliente.MaxLength = 32767;
+            this.TxtBuscarCliente.MouseState = MaterialSkin.MouseState.HOVER;
+            this.TxtBuscarCliente.Name = "TxtBuscarCliente";
+            this.TxtBuscarCliente.PasswordChar = '\0';
+            this.TxtBuscarCliente.SelectedText = "";
+            this.TxtBuscarCliente.SelectionLength = 0;
+            this.TxtBuscarCliente.SelectionStart = 0;
+            this.TxtBuscarCliente.Size = new System.Drawing.Size(363, 23);
+            this.TxtBuscarCliente.TabIndex = 2;
+            this.TxtBuscarCliente.TabStop = false;
+            this.TxtBuscarCliente.UseSystemPasswordChar = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Controls.Add(this.BtnBuscar);
+            this.panel2.Controls.Add(this.TxtBuscarCliente);
+            this.panel2.Location = new System.Drawing.Point(71, 364);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(601, 263);
+            this.panel2.TabIndex = 7;
+            // 
+            // BtnBuscar
+            // 
+            this.BtnBuscar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BtnBuscar.Depth = 0;
+            this.BtnBuscar.Icon = null;
+            this.BtnBuscar.Location = new System.Drawing.Point(464, 12);
+            this.BtnBuscar.MouseState = MaterialSkin.MouseState.HOVER;
+            this.BtnBuscar.Name = "BtnBuscar";
+            this.BtnBuscar.Primary = true;
+            this.BtnBuscar.Size = new System.Drawing.Size(83, 36);
+            this.BtnBuscar.TabIndex = 8;
+            this.BtnBuscar.Text = "Buscar";
+            this.BtnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(37, 67);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(510, 173);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // FrmCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 635);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.BtnEliminar);
+            this.Controls.Add(this.BtnActualizar);
+            this.Controls.Add(this.BtnNuevo);
+            this.Controls.Add(this.LblAdministracionDeClientes);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmCustomer";
+            this.Text = "FrmCustomer";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtNombreCliente;
+        private MaterialSkin.Controls.MaterialLabel LblAdministracionDeClientes;
+        private MaterialSkin.Controls.MaterialSingleLineTextField TxtEmail;
+        private MaterialSkin.Controls.MaterialSingleLineTextField TxtTelefono;
+        private MaterialSkin.Controls.MaterialSingleLineTextField TxtDireccion;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtDocumento;
+        private MaterialSkin.Controls.MaterialRaisedButton BtnNuevo;
+        private MaterialSkin.Controls.MaterialRaisedButton BtnActualizar;
+        private MaterialSkin.Controls.MaterialRaisedButton BtnEliminar;
+        private MaterialSkin.Controls.MaterialSingleLineTextField TxtBuscarCliente;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private MaterialSkin.Controls.MaterialRaisedButton BtnBuscar;
+    }
+}
